650af1241981f70a2dafdd6b6b3316215661fcbc
baglan <sarievbagi@gmail.com>
2022-03-15 17:20:49 +0600

    status change error handle

--- app/Http/Controllers/API/ForteMarketController.php
+++ app/Http/Controllers/API/ForteMarketController.php
@@ -6,18 +6,23 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\OrderStatus;
 use App\Services\MarketPlaces\ForteMarketService;
+use App\Loggers\ErrorLogger;
 
 class ForteMarketController extends Controller
 {
     public function changeOrderStatus(Request $request)
     {
-        if ($request->has(['order_id', 'status_id'])) {
-            $order_status = OrderStatus::findOrFail($request->status_id);
-            $forte_market_service = new ForteMarketService();
+        try {
+            if ($request->has(['order_id', 'status_id'])) {
+                $order_status = OrderStatus::findOrFail($request->status_id);
+                $forte_market_service = new ForteMarketService();
 
-            return response()->json([
-                'success' => $forte_market_service->changeStatus($request->order_id, $order_status->forteMarketOrderStatus->name)
-            ], '200');
+                return response()->json([
+                    'success' => $forte_market_service->changeStatus($request->order_id, $order_status->forteMarketOrderStatus->name)
+                ], '200');
+            }
+        } catch (\Exception $e) {
+            ErrorLogger::write($e);
         }
 
         return response()->json(['success' => false], '404');
